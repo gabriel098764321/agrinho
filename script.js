e3821a49f76a2eff12a97821ca4d0a4857595785
Gabriel Correa <148580196+gabriel098764321@users.noreply.github.com>
2025-06-11 10:30:28 -0300

    5

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ window.addEventListener('load', revealOnScroll);
 
 // Carrossel
 const images = [
-  './img/campo.png',
-  './img/malte.png',
-  './img/cidade.png'
+  './img/carne1.webp',
+  './img/churrasqueira.jpeg',
+  './img/assada.png'
 ];
 let currentIndex = 0;
 
